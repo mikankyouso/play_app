38efa216333119f54e55b1d347936bb454351dc4
mikankyouso <mikankyouso@gmail.com>
2012-03-30 11:30:11 +0900

    a

---
+++ app/assets/javascripts/main.coffee
@@ -0,0 +1,2 @@
+$ ->
+  $("body").append("<span>bbb</span>")
--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,19 +5,20 @@ import play.api.mvc._
 import play.api.data._
 import play.api.data.Forms._
 import models._
+import play.api.templates._
 
 object Application extends Controller {
 
   def index = Action {
     //Ok(views.html.index("あいうえお2 Your new application is ready."))
     //Ok("あいうえお")
-    Redirect(routes.Application.tasks)
+    Ok(views.html.main("たいとる")(Html("aaa")))
   }
 
   val taskForm = Form("label" -> nonEmptyText)
 
   def tasks = Action {
-    Ok(views.html.index(Task.all(), taskForm))
+    Ok(views.html.task(Task.all(), taskForm))
   }
 
   def newTask = Action { implicit request =>
